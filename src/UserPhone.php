945874e624c755631f5d501265b978043a825755
devonblzx <devon.b@virtualcomplete.com>
2017-02-05 15:34:23 -0500

    Prepare the phone number for the database, extract extension from number if an 'x' is given

--- src/UserPhone.php
+++ src/UserPhone.php
@@ -7,4 +7,25 @@ class UserPhone extends Model
 {
     protected $table = 'user_phones';
     protected $guarded = ['id', 'user_id'];
+
+    /**
+     * Prepare the number for the database
+     *   1.  Strip excess characters (ex. hyphens and parentheses) from number
+     *   2.  Extract the extension (looks for an 'x')
+     *
+     * @param $number
+     */
+    public function setNumberAttribute($number)
+    {
+        $number = preg_replace('/[^+\dx]/i', '', $number);
+
+        $extensionPosition = stripos($number, 'x');
+        if ($extensionPosition >= 6) {
+            $this->attributes['extension'] = substr($number, $extensionPosition + 1);
+            $number = substr($number, 0, $extensionPosition);
+        }
+
+        $this->attributes['number'] = $number;
+    }
+
 }
