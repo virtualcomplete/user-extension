152117b608b97670a44b025336171812c3f3ee56
devonblzx <devon.b@virtualcomplete.com>
2017-02-05 15:39:35 -0500

    Remove outdated PHPDoc from trait

--- src/UserExtensionTrait.php
+++ src/UserExtensionTrait.php
@@ -4,20 +4,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Trait UserExtensionTrait
- * To be used on the User model
  *
  * @package VirtualComplete\UserExtension
- *
- * @property string $company_name
- * @property string $address1
- * @property string $address2
- * @property string $city
- * @property string $state
- * @property string $zip
- * @property string $country
- * @property string $phone
- * @property string $language
- * @property string $time_zone
  */
 trait UserExtensionTrait
 {
